21142c09119fa39100fd2cce507c73cc1f50b539
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-29 12:29:46 +0700

    ya

---
+++ switchCase.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int bilSatu, bilDua;
+
+void input() {
+	cout << "masukkan bilangan pertama = ";
+	cin >> bilSatu;
+	cout << "masukkan bilangan kedua = ";
+	cin >> bilDua;
+}
+
+float penjumlahan(float a, float b) {
+	return a + b;
+}
+
+float pembagian(float a, float b) {
+	return a / b;
+}
+
+float perkalian(float a, float b) {
+	return a * b;
+}
+
+float pengurangan(float a, float b) {
+	return a - b;
+}
+
+int main() {
+	int pilihan;
+
+	do {
+		cout << "=======================" << endl;
+		cout << "MENU" << endl;
+		cout << "1. Penjumlahan " << endl;
+		cout << "2. pembagian " << endl;
+		cout << "3. Perkalian " << endl;
+		cout << "4. Pengurangan " << endl;
+		cout << "5. exit" << endl;
+		cout << "pilih pilihan 1/2/3/4/5" << endl;
+		cin >> pilihan;
+
+
+	switch (pilihan) {
+	case 1:
+		input();
+		cout << "hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+		system("pause");
+		break;
+	case 2:
+		input();
+		cout << "hasil pembagian = " << pembagian(bilSatu, bilDua) << endl;
+		system("pause");
+		break;
+	case 3:
+		input();
+		cout << "hasil perkalian = " << perkalian(bilSatu, bilDua) << endl;
+		system("pause");
+		break;
+	case 4:
+		input();
+		cout << "hasil pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+		system("pause");
+		break;
+	case 5:
+		break;
+	default:
+		cout << "pilihan tidak ada" << endl;
+		system("pause");
+		break;
+	}
+	system("CLS");
+	} while (pilihan != 5);
